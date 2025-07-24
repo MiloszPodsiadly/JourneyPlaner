a15794cda5e1b1134bf07cfd35758b2fb7f82337
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-07-24 23:09:44 +0200

    Initial commit

---
+++ src/test/java/com/milosz/podsiadly/journeyplaner/JourneyPlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milosz.podsiadly.journeyplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyPlanerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
