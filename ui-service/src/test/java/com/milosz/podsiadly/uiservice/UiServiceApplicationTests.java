7bb5b10ed2870148d6c2ebf3575ccd1e3a2695a1
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-07-26 22:52:54 +0200

    Version 0.1.0

---
+++ ui-service/src/test/java/com/milosz/podsiadly/uiservice/UiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milosz.podsiadly.uiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
