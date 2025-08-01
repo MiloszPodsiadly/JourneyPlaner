b1dd4f5a7466097c61020d936135574cab01fcff
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-08-01 14:31:26 +0200

    Version 0.2.1

--- ui-service/src/main/frontend/generated/flow/chunks/chunk-69d24a4085d6032ca3392a868036ebc716b76ba2542382d00c8f9e30b1f8bf63.js
+++ ui-service/src/main/frontend/generated/flow/chunks/chunk-69d24a4085d6032ca3392a868036ebc716b76ba2542382d00c8f9e30b1f8bf63.js
@@ -1,10 +1,16 @@
+import 'Frontend/generated/jar-resources/flow-component-renderer.js';
 import '@vaadin/polymer-legacy-adapter/style-modules.js';
 import '@vaadin/text-field/theme/lumo/vaadin-text-field.js';
+import '@vaadin/icons/vaadin-iconset.js';
 import '@vaadin/dialog/theme/lumo/vaadin-dialog.js';
-import 'Frontend/generated/jar-resources/flow-component-renderer.js';
 import '@vaadin/text-area/theme/lumo/vaadin-text-area.js';
 import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
 import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/icon/theme/lumo/vaadin-icon.js';
+import '@vaadin/horizontal-layout/theme/lumo/vaadin-horizontal-layout.js';
 import '@vaadin/button/theme/lumo/vaadin-button.js';
 import 'Frontend/generated/jar-resources/disableOnClickFunctions.js';
+import '@vaadin/select/theme/lumo/vaadin-select.js';
+import 'Frontend/generated/jar-resources/selectConnector.js';
+import 'Frontend/generated/jar-resources/lit-renderer.ts';
 import '@vaadin/notification/theme/lumo/vaadin-notification.js';
---
+++ ui-service/src/main/java/com/milosz/podsiadly/uiservice/dto/SongDto.java
@@ -0,0 +1,3 @@
+package com.milosz.podsiadly.uiservice.dto;
+
+public record SongDto(String artistName, String trackName) {}
