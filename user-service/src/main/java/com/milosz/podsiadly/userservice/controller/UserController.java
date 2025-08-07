3b2f63082b7c0e44b4a946757e815887d17b5093
Milosz Podsiadly <m.podsiadly99@gmail.com>
2025-08-07 19:25:01 +0200

    Updates UserController

--- user-service/src/main/java/com/milosz/podsiadly/userservice/controller/UserController.java
+++ user-service/src/main/java/com/milosz/podsiadly/userservice/controller/UserController.java
@@ -4,6 +4,7 @@ import com.milosz.podsiadly.userservice.dto.CreateUserRequest;
 import com.milosz.podsiadly.userservice.entity.User;
 import com.milosz.podsiadly.userservice.repository.UserRepository;
 
+import com.milosz.podsiadly.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
@@ -17,22 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @PostMapping("/create")
     public ResponseEntity<Void> createUserIfNotExists(@RequestBody CreateUserRequest request) {
-        if (userRepository.existsBySpotifyId(request.spotifyId())) {
-            return ResponseEntity.ok().build();
-        }
-
-        var user = User.builder()
-                .spotifyId(request.spotifyId())
-                .name(request.name())
-                .email(request.email())
-                .build();
-
-        userRepository.save(user);
+        userService.createUserIfNotExists(request);
         return ResponseEntity.ok().build();
     }
 }
+
 
